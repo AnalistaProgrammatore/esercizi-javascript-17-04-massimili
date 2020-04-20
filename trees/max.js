8dc227b2834245b08d3027c41562cee22fa7104d
Massimiliano Puca <max.puca@gmail.com>
2020-04-20 20:21:21 +0200

    esercizi 3 e 4

---
+++ trees/max.js
@@ -0,0 +1,71 @@
+function Node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = show;
+}
+function show() {
+    return this.data;
+}
+function BST() {
+    this.root = null;
+    this.insert = insert;
+    this.inOrder = inOrder;
+    this.cercaMax = cercaMax
+}
+function insert(data) {
+    var n = new Node(data, null, null);
+    if (this.root == null) {
+        this.root = n;
+    }
+    else {
+        var current = this.root;
+        var parent;
+        while (true) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                if (current == null) {
+                    parent.left = n;
+                    break;
+                }
+            }
+            else {
+                current = current.right;
+                if (current == null) {
+                    parent.right = n;
+                    break;
+                }
+            }
+        }
+    }
+}
+function inOrder(node) {
+    if (!(node == null)) {
+        inOrder(node.left);
+        console.log(node.data)
+        inOrder(node.right);
+    }
+}
+function cercaMax(node = nums.root) {
+    if (!(node == null)) {
+        if (node.right != null) {
+            cercaMax(node.right);
+        }
+        else {
+            console.log("Il valore massimo è " + node.data)
+        }
+    }
+}
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal: ");
+inOrder(nums.root)
+cercaMax()
+
